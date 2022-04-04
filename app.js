780bb0a96940e2f31708a675fa8700cacd3bbeac
Ankit <ankit.basantpuria@gmail.com>
2022-04-04 23:39:25 +0530

    added environment varaibale and ignoregit

---
+++ app.js
@@ -0,0 +1,62 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine','ejs');
+app.use(express.static('public'));
+mongoose.connect('mongodb://localhost:27017/userDB',(err)=>{
+  if(!err){console.log("DB connected suucessfully")}
+  else{console.log(err)}
+});
+const userSchema = new mongoose.Schema({
+  username:String,
+  password:String
+});
+var secret = "this is a secret keyfor encrytionaaast";
+userSchema.plugin(encrypt,{secret:secret , encryptedFields:['password']});
+const User = mongoose.model("User",userSchema);
+app.get("/",(req,res)=>{
+  res.render('home');
+});
+app.get("/login",(req,res)=>{
+  res.render('login');
+});
+app.get("/register",(req,res)=>{
+  res.render('register');
+});
+app.post("/register",(req,res)=>{
+  const new_user = new User({
+    username: req.body.username,
+    password: req.body.password
+  })
+  new_user.save((err)=>{
+    if(!err){res.render("secrets");}
+    else{console.log(err);}
+  })
+})
+app.post("/login",(req,res)=>{
+  const email = req.body.username;
+  const password = req.body.password;
+  User.findOne({username:email},(err,founduser)=>{
+    if(!err){
+      if(founduser){
+            if(founduser.password===password){
+              res.render("secrets");
+            }
+            else{
+              console.log("Invalid Password");
+            }
+      }
+      else{console.log("user not found")}
+    }
+    else{
+      console.log(err);
+    }
+  })
+})
+app.listen(3000,()=>{
+  console.log("app listining on port 3000");
+})
